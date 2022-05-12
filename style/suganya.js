8f679b4944be2dcf95ae8bea48394edf80671e63
suganya05 <go2suganya@gmail.com>
2022-05-12 18:08:29 +0530

    background image, search bar created

---
+++ style/suganya.js
@@ -0,0 +1,50 @@
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $(".navbar_brand").addClass("shadow");
+  } else {
+    $(".navbar_brand").removeClass("shadow");
+  }
+});
+
+// close_menu and open_menu
+
+const btn = document.querySelector(".close_menu");
+const bar = document.querySelector(".navbar");
+
+function openMenu() {
+  bar.style.display = "inline";
+}
+function closeMenu() {
+  bar.style.display = "none";
+}
+
+let i = 0;
+let placeholder = "";
+const txt = [
+  "GST Registration",
+  "FSSAI Lisence",
+  "Music License",
+  "E Waste License Recycling",
+  "Company Registration",
+];
+const speed = 100;
+
+const sleep = (ms = speed) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const length = txt.length;
+
+async function type() {
+  for (let j = 0; j < txt.length; j++) {
+    for (let i = 0; i < txt[j].length; i++) {
+      placeholder += txt[j].charAt(i);
+      document
+        .getElementById("search-box")
+        .setAttribute("placeholder", placeholder);
+      await sleep();
+    }
+    placeholder = "";
+    await sleep(500);
+  }
+  type();
+}
+type();
