8312e01ba6ce5c2a62e9c34e74bed59cdbaf9dcf
Subaa2128 <subaa86@gmail.com>
2022-05-12 16:13:15 +0530

    search bar with auto text

--- styles/subaa.js
+++ styles/subaa.js
@@ -19,3 +19,32 @@ function closeMenu() {
     bar.style.display="none"
 
 }
+
+
+
+
+let i = 0;
+let placeholder = '';
+const txt = ["GST Registration","FSSAI Lisence","Music License","E Waste License Recycling","Company Registration"] ;
+const speed = 100;
+
+const sleep=(ms=speed)=>new Promise(resolve=>setTimeout(resolve,ms))
+
+const length=txt.length
+
+async function type(){
+   for (let j = 0; j < txt.length; j++) {
+    for (let i = 0; i < txt[j].length; i++) {
+        placeholder += txt[j].charAt(i);
+        document.getElementById("search-box").setAttribute("placeholder",placeholder);
+        await sleep()  
+       }
+       placeholder=''
+       await sleep(500)
+   }
+   console.log(
+    txt.repeat(5)
+);
+}
+
+type();
