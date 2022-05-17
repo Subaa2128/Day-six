397ea3325ae5cbdd75516fd837a2062fd721bc5a
Subaa2128 <subaa86@gmail.com>
2022-05-17 12:47:55 +0530

    latest news slider with media query done

--- styles/subaa.js
+++ styles/subaa.js
@@ -48,18 +48,50 @@ async function type(){
 type();
 
 
-
-
-
-
-
 $(document).ready(function(){
-   $('.logos').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
+  $('.logos').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    responsive:[
+        {
+            breakpoint:900,
+            settings:{
+                slidesToShow:3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+        }
+    ]
   });
 
-  
+  $('.star-slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay:true,
+    slidesToShow: 1,
+    arrows:false,
+  });
+  });
+
+  $(document).ready(function(){
+    $('.slider').slick({
+      slidesToShow: 1,
+      dots: true,
+      infinite: true,
+      speed: 300,
+      autoplay:true,
+      arrows:false,
+    });
+    });
+
